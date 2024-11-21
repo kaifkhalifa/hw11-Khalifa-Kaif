0a0fa9dde28460f1d7c88c2fe2dabcd27aa07517
Kaif Khalifa <81500937+kaifkhalifa@users.noreply.github.com>
2024-11-21 13:40:16 -0500

    refactor tests.rkt and run function

--- tests.rkt
+++ tests.rkt
@@ -8,24 +8,24 @@
   (test-suite
    "HW11 Tests"
 
-   ;; ---------------------------------------------------------
+
    ;; Tests for `lookup`
-   ;; ---------------------------------------------------------
+
    (check-equal? (lookup 'z '((x 42) (z 99))) 99)
    (check-equal? (lookup 'x '((x 42) (z 99))) 42)
    ;; Additional Test
    (check-equal? (lookup 'w '((x 42) (z 99))) Undefined-Error)
 
-   ;; ---------------------------------------------------------
+
    ;; Tests for `extend-env`
-   ;; ---------------------------------------------------------
+
    (check-equal? (extend-env 'z 99 '((x 42))) '((z 99) (x 42)))
    ;; Additional Test
    (check-equal? (extend-env 'a 100 '()) '((a 100)))
 
-   ;; ---------------------------------------------------------
+
    ;; Tests for `parse`
-   ;; ---------------------------------------------------------
+
    (check-equal? (parse '(+ 10 (+ 5 3))) 
                  (call (vari '+) (list (num 10) (call (vari '+) (list (num 5) (num 3))))))
    (check-equal? (parse 'z) (vari 'z))
@@ -33,9 +33,9 @@
    (check-equal? (parse '(bind x 5 (+ x 2))) 
                  (bind-ast 'x (num 5) (call (vari '+) (list (vari 'x) (num 2)))))
 
-   ;; ---------------------------------------------------------
+
    ;; Tests for `run`
-   ;; ---------------------------------------------------------
+
    (check-equal? (run (call (vari '+) (list (num 10) (num 20))) initial-env) 30)
    (check-equal? 
     (run (bind-ast 'x (num 5) (bind-ast 'y (num 10) (call (vari '+) (list (vari 'x) (vari 'y))))) '()) 
@@ -46,31 +46,24 @@
     (run (bind-ast 'x (num 5) (bind-ast 'y (call (vari '+) (list (vari 'x) (num 3))) (vari 'y))) '())
     8)
 
-   ;; ---------------------------------------------------------
    ;; Tests for Variadic Addition in `run`
-   ;; ---------------------------------------------------------
+
    (check-equal? (run (call (vari '+) (list (num 1) (num 2) (num 3))) initial-env) 6)
    ;; Additional Test
    (check-equal? (run (call (vari '+) (list (num 4) (num 5) (num 6) (num 7))) initial-env) 22)
 
-   ;; ---------------------------------------------------------
-   ;; Tests for Self-Referencing `bind`
-   ;; ---------------------------------------------------------
-   (check-equal? 
-    (run (bind-ast 'x (call (vari '+) (list (num 2) (vari 'x))) (vari 'x)) '((x 10))) 12)
 
 
-   ;; ---------------------------------------------------------
    ;; Tests for Shadowing
-   ;; ---------------------------------------------------------
+
    (check-equal? (run (bind-ast 'x (num 5) (bind-ast 'x (num 10) (vari 'x))) '()) 10)
    ;; Additional Test
    (check-equal? 
     (run (bind-ast 'x (num 5) (bind-ast 'x (num 10) (bind-ast 'x (num 15) (vari 'x)))) '()) 15)
 
-   ;; ---------------------------------------------------------
+
    ;; Tests for Environment in Nested Bindings
-   ;; ---------------------------------------------------------
+
    (check-equal?
     (run (bind-ast 'x (num 5) (bind-ast 'y (call (vari '+) (list (vari 'x) (num 3))) (vari 'y))) '())
     8)
@@ -79,9 +72,9 @@
     (run (bind-ast 'x (num 5) (bind-ast 'y (num 10) (call (vari '+) (list (vari 'x) (vari 'y))))) '())
     15)
 
-   ;; ---------------------------------------------------------
+
    ;; Tests for `initial-env`
-   ;; ---------------------------------------------------------
+
    (check-equal? (lookup '+ initial-env) +)
    ;; Additional Test
    (check-equal? (lookup '- initial-env) -)
